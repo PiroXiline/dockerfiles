d2e563d1a439f1b1071cb887735604b8f15bfdef
Anton Latukha <anton.latukha@gmail.com>
2015-06-05 13:12:11 +0300

    Building process update

--- run-cb-wsoc.sh
+++ run-cb-wsoc.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-docker run -d -v /opt/couchbase/var --name couchbase-wsoc-dbdata pyro/ubuntu-couchbase-wsoc echo 'Data-only couchbase-wsoc container'
+# Create a data only container
+if docker ps -a|grep -q couchbase-wsoc-dbdata ; then
+    echo 'Couchabse data container found, using existing one'
+else
+    echo 'Container with couchbase data not found. Creating new one'
+    docker run -d -v /opt/couchbase/var --name couchbase-wsoc-dbdata pyro/ubuntu-couchbase-wsoc echo 'Data-only couchbase-wsoc container'
+fi
+# Create a runtime container
+if docker ps -a|grep couchbase-wsoc|grep -q -v couchbase-wsoc-dbdata ; then
+    echo 'couchbase-wsoc execution container found, removing.'
+    echo 'Stopping container couchbase-wsoc.'
+    docker stop couchbase-wsoc
+    echo 'Deleting container couchbase-wsoc.'
+    docker rm couchbase-wsoc
+else echo 'couchbase-wsoc execution container not found.'
+fi
+echo 'Starting couchbase execution container.'
 docker run -d -p 8091:8091 -p 8092:8092 -p 9000:9000 -p 11207:11207 -p 11210:11210 -p 11211:11211 -p 18091:18091 -p 18092:18092 --volumes-from couchbase-wsoc-dbdata --name couchbase-wsoc pyro/ubuntu-couchbase-wsoc
-docker exec -ti couchbase-wsoc /bin/bash
+# docker exec -ti couchbase-wsoc /bin/bash
--- run-nginx-wsoc.sh
+++ run-nginx-wsoc.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Create a data only container
-docker run -d --name nginx-wsoc-data -v /var/cache/nginx -v /usr/share/nginx/html -v /var/log/nginx -v /var/www/html pyro/ubuntu-nginx-wsoc echo 'Data-only container for Nginx'
+if docker ps -a|grep -q nginx-wsoc-data ; then
+    echo 'Nginx data container found, using existing one.'
+else
+    echo 'Container with nginx data not found. Creating new one.'
+    docker run -d --name nginx-wsoc-data -v /var/cache/nginx -v /usr/share/nginx/html -v /var/log/nginx -v /var/www/html pyro/ubuntu-nginx-wsoc echo 'Data-only Nginx container'
+fi
 # Create a runtime container
+if docker ps -a|grep nginx-wsoc|grep -q -v nginx-wsoc-dbdata ; then
+    echo 'nginx-wsoc execution container found, replacing with new one.'
+    echo 'Stopping container nginx-wsoc.'
+    docker stop nginx-wsoc
+    echo 'Deleting container nginx-wsoc.'
+    docker rm nginx-wsoc
+else echo 'nginx-wsoc execution container not found.'
+fi
+echo 'Starting nginx-wsoc execution container.'
 docker run -p 80:80 -p 443:443 -d --volumes-from nginx-wsoc-data --name nginx-wsoc pyro/ubuntu-nginx-wsoc
-#docker exec -it couchbase-wsoc /bin/bash
+#docker exec -it nginx-wsoc /bin/bash
